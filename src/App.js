879a145669fad07c747b9ae37ff2f1bf5891ac67
Kevin <kevindiegofloresantezana@hotmail.com>
2021-11-30 17:00:53 -0400

    tabla crud users

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 
 //import Routes from './routes/Routes';
-import {UsersAdmi} from "../Paginas/UsersAdmi";
+import UsersAdmi from "./Paginas/UsersAdmi";
 import Login from "./Paginas/Login";
-//import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 function App() {
   return (
-    /*<Router>
+    <Router>
       <Routes>
         <Route exact path="/" element={<Login/>}/>
         <Route exact path="/UsersAdmin" element={<UsersAdmi/>}/>
       </Routes>
 
-    </Router>*/
-    <UsersAdmi>
-      
-    </UsersAdmi>
+    </Router>
+    
     
     
   );
--- src/Paginas/UsersAdmi.js
+++ src/Paginas/UsersAdmi.js
@@ -1,9 +1,62 @@
-import React,{useState} from 'react'
+
+import React, { useState,useEffect } from "react";
+import axios from 'axios';
+import { Table, TableContainer, TableHead, TableCell, TableBody,TableRow } from "@mui/material";
+import { Typography } from "@mui/material";
+import { Delete, Edit } from "@mui/icons-material";
+
+
+const baseUrl='https://localhost:44324/api/user'
 function UsersAdmi(){
+    const [data, setData] = useState([]);
+
+    const peticionGet= async()=>{
+        await axios.get(baseUrl)
+        .then(response=>{
+            console.log(response.data);
+            setData(response.data)
+        })
+    }
+    useEffect(async()=>{
+        await peticionGet();
+
+    },[])  
+
     return (
         <div>
-            hola
+            <Typography>Lista de usuarios</Typography>
+            
+           <TableContainer>
+               <Table>
+                   <TableHead>
+                       <TableRow>
+                           <TableCell>Id</TableCell>
+                           <TableCell>Nombre</TableCell>
+                           <TableCell>Email</TableCell>
+                           <TableCell>Contraseña</TableCell>
+                           <TableCell>Acciones</TableCell>
+                       </TableRow>
+                   </TableHead>
+                   <TableBody>
+                       {data.map(user=>(
+                           <TableRow key={user.id}>
+                                <TableCell>{user.id}</TableCell>
+                           <    TableCell>{user.name}</TableCell>
+                                <TableCell>{user.email}</TableCell>
+                                <TableCell>{user.password}</TableCell>
+                                <TableCell>
+                                    <Edit/>
+                                    &nbsp;&nbsp;&nbsp;
+                                    <Delete/>
+                                </TableCell>
+                           </TableRow>
+                          
+                       ))}
+                   </TableBody>
+               </Table>
+           </TableContainer>
+           hola
         </div>
-    )
+    );
 }
 export default UsersAdmi;
